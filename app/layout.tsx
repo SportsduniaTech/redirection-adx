2a8dbf8716eb1cbe2e5aa4024c0faf2e590cd220
sameer bajaj <1978sameerbajaj@gmail.com>
2025-05-22 14:57:31 +0530

    removed delay

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,16 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Suspense fallback={
-          <div className="absolute inset-0 bg-[#000000bf] bg-opacity-50 flex flex-col items-center justify-center z-50">
-              <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4 animate-spin"></div>
-          </div>
-        }>
-          {children}
-        </Suspense>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
       </body>
     </html>
   );
